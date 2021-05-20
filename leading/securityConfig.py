dfbfdbd8bd16f6198f9ac17da6012181fa10b419
ilab-1624 <68988193+ilab-1624@users.noreply.github.com>
2021-05-20 11:34:41 +0800

    Update securityConfig.py

--- leading/securityConfig.py
+++ leading/securityConfig.py
@@ -1,3 +1,6 @@
 EdwareAgentsecurityStatus = "disable"
 TommyAgentsecurityStatus = "enable"
 MaxAgentsecurityStatus = "enable"
+customerRole = 'customerRole'
+staffRole = 'staffRole'
+managerRole = 'managerRole'
